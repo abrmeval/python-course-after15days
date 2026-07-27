9a68fc3243e33cbd7115066b5e0fd95571cdf3ce
Abrahim Mejía Valdez <abrmeval@outlook.com>
2026-07-27 11:25:19 -0600

    feat: add a Readme and other tkinter components

---
+++ README.md
@@ -0,0 +1,3 @@
+# Python course
+The repository contains topics related to the udemy python course and other sources.
+It keeps topics and exercises.
--- udemy_course/dev-27/main.py
+++ udemy_course/dev-27/main.py
@@ -6,17 +6,43 @@ window = tkinter.Tk()
 # We set a title to the window
 window.title("My first GUI program")
 
-#Minimun size of the window
+# Minimun size of the window
 window.minsize(width=500, height=300)
 
 # Label
 
 my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
 
- # To pack the label to the window screen. This actually places the label to the screen
-my_label.pack(side="left")
+# To pack the label to the window screen. This actually places the label to the screen
+# my_label.pack(side="left")
+my_label.pack()
+
+# To configure properties of a component, we can do it in two ways:
+# Get access the prop as it was in a dictionary
+my_label["text"] = "New text"
+# Use the config method to set the different properties
+my_label.config(text="New text")
 
 
+# Button
+def button_clicked():
+    # my_label["text"] = "Buton got clicked"
+    input_txt = input.get()
+    my_label["text"] = input_txt
+    print("I got clicked")
+
+
+button = tkinter.Button(text="Click me", command=button_clicked)
+button.pack()
+
+
+# Entry 
+# To define an input text box
+input = tkinter.Entry(width=10)
+input.pack()
+
+# Return the input as a string
+input.get()
 
 # a loop to keep the window showing up without exiting until closed
 window.mainloop()
