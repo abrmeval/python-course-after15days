6fa952971a2c9119d6abd67575fc2b86eb010633
Abrahim Mejía Valdez <abrmeval@outlook.com>
2026-08-10 10:58:41 -0600

    fix: update nato app to handle exceptions

---
+++ udemy_course/dev-30/nato_app.py
@@ -0,0 +1,25 @@
+import pandas
+
+# Keyword Method with iterrows()
+data_frame = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+
+# TODO 1. Create a dictionary in this format:
+# {"A": "Alfa", "B": "Bravo"}
+dict = {row["letter"]: row["code"] for (index, row) in data_frame.iterrows()}
+# print(dict)
+
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+while True:
+    try :
+        word = input("Enter a word: ")
+        if word:
+            word = word.upper()
+            list = [dict[letter] for letter in word]
+            print(list)
+
+        else:
+            print("NONE")
+        break
+    except KeyError:
+        print ("Sorry, only letters in the alphabet please")
