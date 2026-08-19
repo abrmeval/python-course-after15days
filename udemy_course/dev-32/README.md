e7caef799d80aae97f1f035b01d9a03c07eaff79
Abrahim Mejía Valdez <abrmeval@outlook.com>
2026-08-19 13:34:52 -0600

    feat: add email motivation quote functionality

--- udemy_course/dev-32/README.md
+++ udemy_course/dev-32/README.md
@@ -1,1 +1,39 @@
 # Sending emails with smtplib
+
+```python
+import smtplib
+import os
+from dotenv import load_dotenv
+import datetime as dt
+
+load_dotenv()
+my_email = os.getenv("SENDER_EMAIL")
+password = os.getenv("APP_PASSWORD")
+recipient = os.getenv("RECIPIENT_EMAIL")
+smpt_server = os.getenv("SMTP_SERVER")
+
+# Sending an email with smtplib
+with smtplib.SMTP(smpt_server) as connection:
+    connection.starttls()
+    connection.login(my_email, password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs=recipient,
+        msg="Subject:Hello\n\n This is the body of the message",
+    )
+
+# Handling datetime
+# Returns the current date in an specific format
+now = dt.datetime.now()
+print(now)
+print(now.year)  # => Returns the current year only
+print(now.date())  # => Returns the current Date part only
+print(now.hour)  # => Returns the current hour only
+print(now.time())  # => Returns the time part of the DateTime
+print(
+    now.weekday()
+)  # => returns the current day of the week as an integer (number of the day of the current week)
+
+date = dt.datetime(year=1995, month=12, day=15) # Create a custom DateTime
+print(date)
+```
--- udemy_course/dev-32/main.py
+++ udemy_course/dev-32/main.py
@@ -1,18 +1,37 @@
 import smtplib
 import os
 from dotenv import load_dotenv
+import datetime as dt
+import random
 
-load_dotenv()
-my_email = os.getenv("SENDER_EMAIL")
-password = os.getenv("APP_PASSWORD")
-recipient = os.getenv("RECIPIENT_EMAIL")
-smpt_server = os.getenv("SMTP_SERVER")
+WEEK_DAY = 2
 
-with smtplib.SMTP(smpt_server) as connection:
-    connection.starttls()
-    connection.login(my_email, password)
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs=recipient,
-        msg="Subject:Hello\n\n This is the body of the message",
-    )
+
+def get_quote():
+    with open("quotes.txt") as file:
+        lines = file.readlines()
+        rdm_index = random.randint(0, len(lines))
+        return lines[rdm_index]
+
+
+now = dt.datetime.now()
+current_week_day = now.weekday()
+print("day of the week:", current_week_day)
+
+if current_week_day == WEEK_DAY:
+    load_dotenv()
+    my_email = os.getenv("SENDER_EMAIL")
+    password = os.getenv("APP_PASSWORD")
+    recipient = os.getenv("RECIPIENT_EMAIL")
+    smtp_server = os.getenv("SMTP_SERVER")
+    quote = get_quote()
+    print("quote:", quote)
+
+    with smtplib.SMTP(smtp_server) as connection:
+        connection.starttls()
+        connection.login(my_email, password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=recipient,
+            msg=f"Subject:Happy Wendnesday\n\n{quote}",
+        )
